805e4c5161562b53bd26d45c0d8a90a048ee0c9d
ALEXANDER DYNDIN <like81@yandex.ru>
2020-12-20 15:51:14 +0300

    desgn update

--- src/features/common/components/HeaderLink.vue
+++ src/features/common/components/HeaderLink.vue
@@ -1,51 +1,44 @@
 <template>
     <div>
-        <router-link
-            :to="to"
-            class="nav-link"
-            :class="active ? 'font-weighted-bold' : null"
-        >
+        <router-link :to="to"
+                     class="nav-link"
+                     v-bind:class="active ? 'font-weighted-bold' : null">
             <slot />
         </router-link>
     </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import { useRoute } from "vue-router";
-import { computed } from "@vue/reactivity";
-
-type HeaderLinkProps = {
-    to: string;
-    exact: boolean;
-};
-
-const HeaderLink = defineComponent({
-    props: {
-        to: { type: String, required: true },
-        exact: { type: Boolean, default: false }
-    },
-    setup(props: HeaderLinkProps) {
-        const route = useRoute();
-        const active = computed(() =>
-            props.exact
-                ? route.path === props.to
-                : route.path.startsWith(props.to)
-        );
-
-        return { active };
-    }
-});
-
-export default HeaderLink;
+    import { defineComponent } from "vue";
+    import { useRoute } from "vue-router";
+    import { computed } from "@vue/reactivity";
+    type HeaderLinkProps = {
+        to: string;
+        exact: boolean;
+    };
+    const HeaderLink = defineComponent({
+        props: {
+            to: { type: String, required: true },
+            exact: { type: Boolean, default: false }
+        },
+        setup(props: HeaderLinkProps) {
+            const route = useRoute();
+            const active = computed(() =>
+                props.exact
+                    ? route.path === props.to
+                    : route.path.startsWith(props.to)
+            );
+            return { active };
+        }
+    });
+    export default HeaderLink;
 </script>
 
 <style scoped>
-.nav-link {
-    font-size: 22px;
-    text-decoration: none;
-    color: cadetblue;
-
-    margin: 0 6px;
-}
+    .nav-link {
+        font-size: 22px;
+        text-decoration: none;
+        color: cadetblue;
+        margin: 0 6px;
+    }
 </style>
--- src/features/common/router/router.ts
+++ src/features/common/router/router.ts
@@ -1,7 +1,7 @@
 ﻿import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 import Home from "@/features/home/views/Home.vue";
 import Schedule from "@/features/schedule/views/Schedule.vue";
-import Deadlines from "@/features/deadlines/views/Deadlines.vue";
+import Deadlines from "@/features/account/deadlines/views/Deadlines.vue";
 import Search from "@/features/search/views/Search.vue";
 import Auth from "@/features/account/auth/views/Auth.vue";
 import TeachersSearch from "@/features/account/teachersSearch/views/TeachersSearch.vue";
