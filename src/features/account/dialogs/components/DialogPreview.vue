5a470b0ffe9239071f689d1b4e9afd72779a895c
Artem Mikhalev <thenostereal@gmail.com>
2020-12-26 22:36:01 +0300

    wip dialogs list

---
+++ src/features/account/dialogs/components/DialogPreview.vue
@@ -0,0 +1,30 @@
+<template>
+    <div @click="openDialog">
+        <img :src="dialog.avatarUrl" />
+        <p>{{ dialog.authorName }}</p>
+        <p>{{ dialog.authorGroup }}</p>
+        <p>{{ dialog.message }}</p>
+        
+        <p>{{ dialog.date }}</p>
+    </div>
+</template>
+
+<script lang="ts">
+import DialogPreview from "@/domain/account/dialogs/model/dialogs";
+import { defineComponent, PropType } from "vue";
+
+const DialogPreview = defineComponent({
+    props: {
+        dialog: Object as PropType<DialogPreview>
+    },
+    methods: {
+        openDialog() {
+
+        }
+    }
+});
+
+export default DialogPreview;
+</script>
+
+<style scoped></style>
---
+++ src/features/account/dialogs/views/DialogsList.vue
@@ -0,0 +1,23 @@
+<template>
+    
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import DialogsUseCase from "@/domain/account/dialogs/usecase/dialogsUseCase";
+
+const useCase = new DialogsUseCase();
+
+const DialogsList = defineComponent({
+    components: {},
+    setup() {
+
+    }
+});
+
+export default DialogsList;
+</script>
+
+<style scoped>
+
+</style>
