047f4c01b4d83bf8d3345fca3b29694da3e01044
Artem Mikhalev <thenostereal@gmail.com>
2020-12-26 13:47:25 +0300

    added data and domain layers

---
+++ src/data/account/applications/remote/applicationsRemoteDataSource.ts
@@ -0,0 +1,11 @@
+﻿import Result from "../../../../utils/result";
+import ApplicationsApi from "../api/applicationsApi";
+import Application from "@/domain/account/applications/model/application";
+
+export default class ApplicationsRemoteDataSource {
+    private api = new ApplicationsApi();
+
+    getApplications(): Promise<Result<Application[]>> {
+        return this.api.getApplications();
+    }
+}
---
+++ src/domain/account/applications/usecase/applicationsUseCase.ts
@@ -0,0 +1,11 @@
+﻿import ApplicationsRepository from "@/data/account/applications/repository/applicationsRepository";
+import Result from "@/utils/result";
+import Application from "../model/application";
+
+export default class ApplicationsUseCase {
+    private repository = new ApplicationsRepository();
+
+    getApplications(): Promise<Result<Application[]>> {
+        return this.repository.getAccountInfo();
+    }
+}
